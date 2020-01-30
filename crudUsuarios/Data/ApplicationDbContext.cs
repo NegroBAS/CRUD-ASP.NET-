742ef49a58858113636003ded11813e473e927fc
NegroBAS <sb1010.001@gmail.com>
2020-01-29 23:02:17 -0500

    CRUD

---
+++ crudUsuarios/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using crudUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudUsuarios.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+    }
+}
---
+++ crudUsuarios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace crudUsuarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
